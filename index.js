3af00a9dbc94fc50c2d88d67bed3ad998084d611
Joanderson <168788764+dinhonks@users.noreply.github.com>
2024-09-15 12:59:22 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -58,11 +58,25 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices:[...realizadas]
     })
+}
 
-    console.log(realizadas)
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) =>{
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message:"Metas Abertas " + abertas.length,
+        choices:[...abertas]
+    })
 }
 
 const start = async() => {
@@ -86,6 +100,11 @@ const start = async() => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -104,6 +123,9 @@ const start = async() => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima")
