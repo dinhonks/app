29d69706b21cb527f5e68f0bdfcc9b5d4d8c29e6
Joanderson <168788764+dinhonks@users.noreply.github.com>
2024-09-14 22:07:58 -0300

    resolve problema marcados/desmarcados

--- aula.md
+++ aula.md
@@ -38,12 +38,14 @@
 - [x] Operadores de atribuição de valor: =
 - [x] Operador de concatenação: +
 - [x] Operadores de comparação: == != <= >= < >
+- [x] Spread operator: ...
 
 ## Estruturas de dados:
 
 ### Arrays []:
 
 - [x] Uma lista que contém qualquer tipo de dado
+- [x] Métodos de array: push, [find, forEach, filter] : HOF (higher Order Functios)
 
 ### Objetos {}:
 
--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const listarMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+    
     if(respostas.length == 0){
         console.log("Nenhuma meta selecionada!")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) =>{
         const meta = metas.find((m) => {
@@ -45,6 +45,24 @@ const listarMetas = async () => {
     })
 
     console.log('Meta(s) marcada(s) como  concluída(s)')
+}
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0){
+        console.log("Não existem metas realizadas! :(" )
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices:[...realizadas]
+    })
+
+    console.log(realizadas)
 }
 
 const start = async() => {
@@ -64,6 +82,10 @@ const start = async() => {
 
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -79,6 +101,9 @@ const start = async() => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima")
