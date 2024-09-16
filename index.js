44ad8e9b5469a4c029fa7d8b4c0cd5255026ac6b
Joanderson <168788764+dinhonks@users.noreply.github.com>
2024-09-15 21:32:05 -0300

    adiciona comentários

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
+/* Importa as funções 'select', 'input' e 'checkbox' do módulo '@inquirer/prompts'
+    Essas funções são usadas para criar prompts interativos para o usuário*/
+
 const { select, input, checkbox } = require('@inquirer/prompts')
+
+// Importa o módulo 'fs' com suporte a promessas para operações assíncronas de leitura e escrita de arquivos
 const fs = require("fs").promises
 
+// Inicializa a variável 'mensagem' com uma mensagem de boas-vindas
 let mensagem = "Bem vido ao app de Metas";
 
+/* Declara a variável 'metas' que será usada para armazenar as metas carregadas do arquivo JSON
+    Inicialmente, 'metas' não tem um valor definido e será preenchida mais tarde*/
 let metas
 
+// Função para carregar as metas do arquivo JSON
 const carregarMetas = async () => {
     try {
+
+        // Tenta ler o arquivo metas.json e parsear o conteúdo como JSON
         const dados = await fs.readFile ("metas.json", "utf-8")
         metas = JSON.parse(dados)
     }
     catch(erro){
+        
+        // Se ocorrer um erro (por exemplo, arquivo não encontrado), inicializa metas como um array vazio
         metas = []
     }
 }
 
+// Função para salvar as metas no arquivo JSON
 const salvarMetas = async () => {
+
+    // Converte o array de metas para uma string JSON e escreve no arquivo metas.json
     await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
 }
 
+// Função para cadastrar uma nova meta
 const cadastrarMeta = async () => {
+
+    // Solicita ao usuário a entrada de uma nova meta
     const meta = await input({message: "Digite a meta:"})
 
+    // Verifica se a meta está vazia e define uma mensagem de erro se necessário
     if (meta.length == 0) {
         mensagem = "A meta não pode ser vazia."
         return
     }
 
+    // Adiciona a nova meta ao array de metas com status "não concluído"
     metas.push(
         {value: meta, checked: false}
     )
@@ -34,26 +55,33 @@ const cadastrarMeta = async () => {
     mensagem = "Meta cadastrada com sucesso!"
 }
 
+// Função para listar e marcar/desmarcar metas
 const listarMetas = async () => {
+
+    // Verifica se há metas para listar
     if(metas.length == 0){
         mensagem = "Não existem metas!"
         return
     }
+    // Exibe um menu de checkboxes para o usuário selecionar metas
     const respostas = await checkbox({
         message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
         choices:[...metas],
         instructions: false,
     })
 
+    // Desmarca todas as metas
     metas.forEach((m) => {
         m.checked = false
     })
     
+    // Se nenhuma meta foi selecionada, define uma mensagem de erro
     if(respostas.length == 0){
         mensagem = "Nenhuma meta selecionada!"
         return
     }
 
+    // Marca as metas selecionadas como concluídas
     respostas.forEach((resposta) =>{
         const meta = metas.find((m) => {
             return m.value == resposta
@@ -65,66 +93,87 @@ const listarMetas = async () => {
     mensagem = 'Meta(s) marcada(s) como  concluída(s)'
 }
 
+// Função para exibir as metas realizadas
 const metasRealizadas = async () => {
+
+     // Verifica se há metas para mostrar
     if(metas.length == 0){
         mensagem = "Não existem metas!"
         return
     }
+     // Filtra as metas que estão marcadas como concluídas
     const realizadas = metas.filter((meta) => {
         return meta.checked
     })
 
+    // Se nenhuma meta foi realizada, define uma mensagem de erro
     if(realizadas.length == 0){
         mensagem = "Não existem metas realizadas! :("
         return
     }
 
+    // Exibe as metas realizadas ao usuário
     await select({
         message: "Metas Realizadas " + realizadas.length,
         choices:[...realizadas]
     })
 }
 
+// Função para exibir as metas abertas
 const metasAbertas = async () => {
+
+    // Verifica se há metas para mostrar
     if(metas.length == 0){
         mensagem = "Não existem metas!"
         return
     }
+
+    // Filtra as metas que não estão marcadas como concluídas
     const abertas = metas.filter((meta) =>{
         return meta.checked != true
     })
 
+    // Se nenhuma meta está aberta, define uma mensagem de sucesso
     if(abertas.length == 0){
         mensagem = "Não existem metas abertas! :)"
         return
     }
 
+    // Exibe as metas abertas ao usuário
     await select({
         message:"Metas Abertas " + abertas.length,
         choices:[...abertas]
     })
 }
 
+// Função para deletar metas selecionadas
 const deletarMetas = async () => {
+
+    // Verifica se há metas para deletar
     if(metas.length == 0){
         mensagem = "Não existem metas!"
         return
     }
+
+    // Cria uma cópia das metas desmarcadas para exibição
     const metasDesmarcadas = metas.map((meta) => {
         return {value: meta.value, checked: false }
     })
 
+    // Exibe um menu de checkboxes para o usuário selecionar metas a serem deletadas
     const itemsADeletar = await checkbox({
         message: "Selecionar item para deletar",
         choices: [...metasDesmarcadas],
         instructions:false,
     })
 
+    // Se nenhuma meta foi selecionada para deletar, define uma mensagem de erro
     if(itemsADeletar.length == 0){
         mensagem = "Nenhum item para deletar!"
         return
     }
 
+    // Remove as metas selecionadas do array de metas
     itemsADeletar.forEach((item) => {
         metas = metas.filter((meta) => {
             return meta.value != item
@@ -133,6 +182,7 @@ const deletarMetas = async () => {
     mensagem = "Meta(s) deletada(s) com sucesso!"
 }
 
+// Função para exibir a mensagem atual
 const mostrarMensagem = () => {
     console.clear();
 
@@ -144,13 +194,16 @@ const mostrarMensagem = () => {
     }
 }
 
+// Função principal para iniciar o aplicativo
 const start = async() => {
     await carregarMetas()
 
     while(true){
+        // Exibe a mensagem atual e salva as metas no arquivo JSON
        mostrarMensagem()
        await salvarMetas()
 
+       // Exibe o menu principal e aguarda a seleção do usuár
         const opcao =  await select({
             message: "Menu >",
             choices: [
@@ -184,7 +237,7 @@ const start = async() => {
         })
 
 
-
+        // Executa a ação correspondente à opção selecionada
         switch(opcao){
             case "cadastrar":
                 await cadastrarMeta()
@@ -209,4 +262,5 @@ const start = async() => {
     }
 }
 
+// Inicia o aplicativo
 start()
